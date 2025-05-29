b8bf8d04e32f116da19f48642a072ce184fafe2a
Dexter2099 <164971293+Dexter2099@users.noreply.github.com>
2025-05-29 13:44:10 +1000

    fix: resolve prettier violations

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -46,8 +46,7 @@ export default function Hero() {
         />
       </div>
       <p className="mx-auto mt-3 max-w-lg text-gray-500 dark:text-gray-400 md:text-xl lg:text-lg xl:text-xl">
-        Welcome to my personal blog where I write about my software engineering
-        journey.
+        Welcome to my personal blog where I write about my software engineering journey.
       </p>
       <p className="mx-auto max-w-lg text-gray-500 dark:text-gray-400 md:text-xl lg:text-lg xl:text-xl">
         I'm based in Brisbane, Australia but open to work remotely!
